26f15b967d91617026f30bd2514cae82c63d6245
徐叶伟 <xuyw@jec.com.cn>
2021-01-19 23:01:20 +0800

    页面发布消费方

---
+++ xc-service-manage-cms-client/src/test/java/com/xuecheng/manage_cms_client/XcServiceManageCmsClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xuecheng.manage_cms_client;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XcServiceManageCmsClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
